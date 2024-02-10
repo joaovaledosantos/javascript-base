3861e5cbdbc32394ea60e2f95bccf789b99371e8
João V. Santos <joaovaledosantos@gmail.com>
2024-02-09 23:02:43 -0300

    [Aula-73] Operadores aritméticos

---
+++ script-73.js
@@ -0,0 +1,19 @@
+  // console.log('Adiçao (+):', 2 + 2);
+
+  // console.log('subtraçao (-):', 8 - 3);
+
+  // console.log('Divisao (/):', 12 / 2);
+
+  // console.log('Multiplicaçao (*):', 5 * 8);
+
+  // console.log('Modulo (%):', 10 % 2);
+
+
+  // var valor = 10;
+  // console.log('Incremento (++):', ++valor);
+  // console.log(valor);
+
+  // console.log('Decretamento (--):', --valor);
+  // console.log(valor);
+
+  console.log('Exponenciaçao (**):', 2 ** 3);
